a660d925fbe346fca4e7162edb3cb3abbe961456
daeinc <daeinc@aol.com>
2019-12-06 10:12:39 -0600

    Initial Commit - C# Winform

---
+++ OLO Pizza Challenge/FormMain.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace OLO_Pizza_Challenge
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button_ShowPopularCombinations = new System.Windows.Forms.Button();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button_ShowPopularCombinations
+            // 
+            this.Button_ShowPopularCombinations.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_ShowPopularCombinations.Location = new System.Drawing.Point(30, 10);
+            this.Button_ShowPopularCombinations.Name = "Button_ShowPopularCombinations";
+            this.Button_ShowPopularCombinations.Size = new System.Drawing.Size(182, 23);
+            this.Button_ShowPopularCombinations.TabIndex = 1;
+            this.Button_ShowPopularCombinations.Text = "Show Popular Combinations";
+            this.Button_ShowPopularCombinations.UseVisualStyleBackColor = true;
+            this.Button_ShowPopularCombinations.Click += new System.EventHandler(this.Button_ShowPopularCombinations_Click);
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.AllowUserToAddRows = false;
+            this.dgvResults.AllowUserToDeleteRows = false;
+            this.dgvResults.AllowUserToResizeRows = false;
+            this.dgvResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Location = new System.Drawing.Point(30, 41);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.ReadOnly = true;
+            this.dgvResults.RowHeadersVisible = false;
+            this.dgvResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvResults.Size = new System.Drawing.Size(303, 468);
+            this.dgvResults.TabIndex = 3;
+            this.dgvResults.Visible = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 517);
+            this.Controls.Add(this.dgvResults);
+            this.Controls.Add(this.Button_ShowPopularCombinations);
+            this.Name = "FormMain";
+            this.Text = "OLO Pizza Challenge - Daniel Ewald";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_ShowPopularCombinations;
+        private System.Windows.Forms.DataGridView dgvResults;
+    }
+}
+
---
+++ OLO Pizza Challenge/FormMain.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+
+namespace OLO_Pizza_Challenge
+{
+    public partial class FormMain : Form
+    {
+        string sURL = @"http://files.olo.com/pizzas.json";
+
+        #region CONSTRUCTOR
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region private void Button_ShowPopularCombinations_Click(object sender, EventArgs e)
+        private void Button_ShowPopularCombinations_Click(object sender, EventArgs e)
+        {
+            string sJSON = "";
+            List<PizzaOrder> lstPizzasOrdered;
+            List<PizzaVariety> lstPizzaVarieties;
+            const int iMAX_RESULTS = 20;
+
+            // HIDE ANY PRIOR RESULTS IN CASE OF AN ERROR ON A SUBSEQUENT RUN
+            dgvResults.Visible = false;
+
+            #region DOWNLOAD PIZZA ORDERS JSON, IF ERROR SHOW MESSAGE, RETURN
+            try
+            {
+                sJSON = WebRequestGetJSON(sURL);
+            }
+            catch
+            {
+                MessageBox.Show("Error downloading pizza order data, please try again.");
+
+                return;
+            }
+            #endregion
+
+            #region DESERIALIZE JSON INTO LIST OF PIZZAS ORDERED
+            try
+            {
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                lstPizzasOrdered = jss.Deserialize<List<PizzaOrder>>(sJSON);
+            }
+            catch
+            {
+                MessageBox.Show("Error converting pizza order data.  An OLO representative will be in contact to assist you");
+                
+                // IDEALLY TRIGGER NOTIFICATION TO SUPPORT STAFF
+                return;
+            }
+
+            //MessageBox.Show("lstPizzasOrdered.Count: " + lstPizzasOrdered.Count);
+            #endregion
+
+            #region GROUP THE PIZZAS ORDERED BY NORMALIZED TOPPING COMBINATION, SORT BY ORDER COUNTS (HIGHEST FIRST)
+            lstPizzaVarieties = lstPizzasOrdered.GroupBy(po => po.GetNormalizedToppingCombination())
+                .Select(pv => new PizzaVariety
+                    {
+                        ToppingCombination = pv.Key,
+                        OrderCount = pv.Count()
+                    }
+                )
+                .OrderByDescending(pvo => pvo.OrderCount).ToList();
+            #endregion
+
+            #region LIMIT THE RESULTS SET
+            if (lstPizzaVarieties.Count > iMAX_RESULTS)
+            {
+                lstPizzaVarieties.RemoveRange(iMAX_RESULTS, lstPizzaVarieties.Count - iMAX_RESULTS);
+            }
+            #endregion
+
+            #region SET THE POPULARITY RANKS (ORDINALLY) 1-BASED
+            for (int iIndex = 0; iIndex < lstPizzaVarieties.Count; iIndex++)
+            {
+                lstPizzaVarieties[iIndex].Rank = iIndex + 1;
+            }
+            #endregion
+
+            // BIND THE RESULT SET TO RESULTS DATA GRID VIEW
+            dgvResults.DataSource = lstPizzaVarieties;
+
+            // SHOW THE RESULTS
+            dgvResults.Visible = true;
+        }
+        #endregion
+
+
+        #region private static string WebRequestGetJSON(string sURL)
+        private static string WebRequestGetJSON(string sURL)
+        {
+            string sResponse = "";
+
+            HttpWebRequest webReq = (HttpWebRequest)HttpWebRequest.Create(sURL);
+            webReq.Method = WebRequestMethods.Http.Get;
+            webReq.Accept = "application/json; charset=utf-8";
+
+            HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("HttpStatusCode is not OK");
+            }
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    sResponse = streamReader.ReadToEnd();
+                }
+            }
+
+            return sResponse;
+        }
+        #endregion
+
+    }
+
+
+    #region PizzaOrder CLASS
+    public class PizzaOrder
+    {
+        public List<string> Toppings { get; set; }
+
+        public string GetNormalizedToppingCombination()
+        {
+            // SORT THE TOPPINGS FOR PROPER GROUPING
+            List<string> lstSortedToppings = Toppings.OrderBy(t => t).ToList();
+
+            // PUT IN LOWER CASE COMMA DELIMITED STRING
+            string sResult = string.Join(", ", lstSortedToppings).ToLower();
+
+            return sResult;
+        }
+    }
+    #endregion
+
+    #region PizzaVariety CLASS
+    public class PizzaVariety
+    {
+        public int Rank { get; set; }
+        public string ToppingCombination { get; set; }
+        public int OrderCount { get; set; }
+    }
+    #endregion
+
+}
